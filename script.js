a492c2dfecea63ecadfb6b1c6f1243e5abf7a3a8
kovvas <alejandromcht@gmail.com>
2023-07-28 21:24:45 -0500

    scroll, paleta_colores, animacion

--- script.js
+++ script.js
@@ -1,43 +1,45 @@
 $(document).ready(function() {
-    var lazyLoadInstance = new LazyLoad(); // Inicializamos la librería LazyLoad
-  
+  var lazyLoadInstance = new LazyLoad({
+    elements_selector: ".lazy"
+  });
+
+  $('.fade-up').each(function() {
+    var bottom_of_element = $(this).offset().top + $(this).outerHeight() - 100;
+    var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+    if (bottom_of_window > bottom_of_element) {
+      $(this).addClass('visible');
+    }
+  });
+
+  $(window).scroll(function() {
     $('.fade-up').each(function() {
       var bottom_of_element = $(this).offset().top + $(this).outerHeight() - 100;
       var bottom_of_window = $(window).scrollTop() + $(window).height();
-  
+
       if (bottom_of_window > bottom_of_element) {
         $(this).addClass('visible');
       }
     });
-  
-    $(window).scroll(function() {
-      $('.fade-up').each(function() {
-        var bottom_of_element = $(this).offset().top + $(this).outerHeight() - 100;
-        var bottom_of_window = $(window).scrollTop() + $(window).height();
-  
-        if (bottom_of_window > bottom_of_element) {
-          $(this).addClass('visible');
-        }
-      });
-    });
-  
-    $(window).scroll(function() {
-      $('.scroll-down-reveal').each(function() {
-        var bottom_of_element = $(this).offset().top + $(this).outerHeight() - 100;
-        var bottom_of_window = $(window).scrollTop() + $(window).height();
-  
-        if (bottom_of_window > bottom_of_element) {
-          $(this).addClass('visible');
-        }
-      });
-    });
-  
-    // Código para cambiar el color de fondo del header en scroll
-    $(window).scroll(function() {
-      if ($(window).scrollTop() > 100) {
-        $('header').css('background-color', '#fff');
-      } else {
-        $('header').css('background-color', '#f7f0e9');
+  });
+
+  $(window).scroll(function() {
+    $('.scroll-down-reveal').each(function() {
+      var bottom_of_element = $(this).offset().top + $(this).outerHeight() - 100;
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+      if (bottom_of_window > bottom_of_element) {
+        $(this).addClass('visible-scroll-down');
       }
     });
   });
+
+  // Código para cambiar el color de fondo del header en scroll
+  $(window).scroll(function() {
+    if ($(window).scrollTop() > 100) {
+      $('header').css('background-color', '#fff');
+    } else {
+      $('header').css('background-color', '#f7f0e9');
+    }
+  });
+});
